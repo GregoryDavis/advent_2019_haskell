115e6247506704a96fd7d7c2c33c22a7333e0afe
Greg <gregory.a.davis7@gmail.com>
2019-12-13 03:52:14 -0500

    Updated solution to account for fuel mass.  Day 1 complete.

--- day_1/part_2/rocket.hs
+++ day_1/part_2/rocket.hs
@@ -5,5 +5,10 @@ main = do
         contents <- readFile "input.txt"
         print . foldl (\acc x -> acc + fuelCalc x) 0 . map (read::String->Int) . words $ contents
 
+-- Fuel calculation for component mass + the fuel required to transport it
 fuelCalc :: Integral a => a -> a
-fuelCalc = subtract 2 . (`div` 3)
+fuelCalc payload 
+    | payload <= 0 = 0
+    | otherwise    = fuel + fuelCalc fuel
+    where fuel = max 0 ( ( payload `div` 3) - 2 )
+    
