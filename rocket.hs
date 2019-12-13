b7723393087c85992d49ea09d31a405aea7e694b
Greg <gregory.a.davis7@gmail.com>
2019-12-13 03:23:31 -0500

    Removed top level copies of day_1

--- rocket.hs
+++
@@ -1,9 +0,0 @@
-import System.IO
-
-main :: IO ()
-main = do  
-        contents <- readFile "input.txt"
-        print . foldl (\acc x -> acc + fuelCalc x) 0 . map (read::String->Int) . words $ contents
-
-fuelCalc :: Integral a => a -> a
-fuelCalc = subtract 2 . (`div` 3)
